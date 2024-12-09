8adf813b22d7f7d44bfe7c80c1cbddb1ad01843f
haominhnguyen <haodz.top@gmail.com>
2024-12-09 17:40:12 +0700

    fix file path

--- src/main/java/com/fu/pha/configuration/FirebaseConfig.java
+++ src/main/java/com/fu/pha/configuration/FirebaseConfig.java
@@ -1,13 +1,18 @@
 package com.fu.pha.configuration;
 
+import com.fu.pha.exception.BadRequestException;
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.net.URL;
+import java.util.Optional;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -19,9 +24,17 @@ public class FirebaseConfig {
     @Bean
     public FirebaseApp firebaseApp() throws IOException {
         logger.info("Đang khởi tạo Firebase...");
+        ClassLoader classloader = Thread.currentThread().getContextClassLoader();
+        URL url = classloader.getResource("pharmacy-management-e6dd2-firebase-adminsdk-8bp3f-52aeed4337.json");
 
-        FileInputStream serviceAccount =
-                new FileInputStream("src/main/resources/pharmacy-management-e6dd2-firebase-adminsdk-8bp3f-52aeed4337.json");
+        final File fireSetting;
+        if (url != null) {
+            fireSetting = new File(url.getFile());
+        } else {
+            logger.error("Not exist file~~~");
+            throw new BadRequestException("File not found!!!");
+        }
+        FileInputStream serviceAccount = new FileInputStream(fireSetting);
 
         FirebaseOptions options = FirebaseOptions.builder()
                 .setCredentials(GoogleCredentials.fromStream(serviceAccount))
